5d80ec195a62eb68737e757c085e1cd71bddc4c9
Marie Christine Henniges <m.henniges@gmail.com>
2020-09-25 10:54:18 +0100

    enhanced datasets and documentation

--- data-raw/process.R
+++ data-raw/process.R
@@ -2,19 +2,97 @@
 
 #base list
 
-BIlist = read.csv("data-raw/BI_MAIN.csv")
+main = read.csv("data-raw/BI_MAIN.csv")
 
-BIlist$StaceIV_nativity = as.factor(BIlist$StaceIV_nativity)
+checklist = dplyr::select(main, 
+                          kew_id, 
+                          taxon_name, 
+                          taxon_name_Stace, 
+                          authors_Stace, 
+                          order, 
+                          family, 
+                          genus, 
+                          subgenus, 
+                          section, 
+                          subsection, 
+                          series, 
+                          species, 
+                          group, 
+                          aggregate, 
+                          members_of_agg., 
+                          taxonomic_status, 
+                          WCVP.URL, 
+                          POWO.URL, 
+                          IPNI.URL, 
+                          accepted.WCVP.URL)
+
+checklist$taxonomic_status = as.factor(checklist$taxonomic_status)
+
+
+main = dplyr::select(main, 
+                     kew_id, 
+                     unclear_species_marker, 
+                     taxon_name, 
+                     taxon_name_Stace, 
+                     authors_Stace, 
+                     taxon_name_WCVP, 
+                     authors_WCVP,
+                     order,
+                     family,
+                     genus,
+                     subgenus,
+                     section,
+                     subsection,
+                     series,
+                     species,
+                     group,
+                     aggregate,
+                     members_of_agg.,
+                     taxonomic_status,
+                     accepted_kew_id,
+                     accepted_name,
+                     accepted_authors,
+                     imperfect.match.with.Stace.IV,
+                     WCVP.URL,
+                     POWO.URL,
+                     IPNI.URL,
+                     accepted.WCVP.URL,
+                     StaceIV_nativity,
+                     Atlas_nativity.viaALIENATT_PLANTATT.,
+                     Stace_Crawley_nativity.aliens.,
+                     origin,
+                     broad_continental_origin,
+                     life_form,
+                     biome,
+                     GB_Man_hectad_level_records_post_2000,
+                     Ireland_hectad_level_records_post_2000,
+                     Channel_Islands_hectad_level_records_post_2000,
+                     IUCN_authority,
+                     IUCN_assessmentId,
+                     IUCN_interlTaxonId,
+                     IUCN_redlistCategory,
+                     IUCN_redlistCriteria,
+                     IUCN_criteriaVersion,
+                     IUCN_populationTrend,
+                     IUCN_scopes)
+                     
+                     
+                     
+                     
+
+main$StaceIV_nativity = as.factor(BIlist$StaceIV_nativity)
+
+
 
 
 
 #Chromosome counts
 
-Chrom_num = read.csv("data-raw/Chrom_raw.csv")
+chrom_num = read.csv("data-raw/Chrom_raw.csv")
 
-Chrom_num = dplyr::select(Chrom_num, LIST, TAXONCITED, chromosome_count, notes, unambiguous_match, matched_by_cit., match_to_subsp_etc.)
+chrom_num = dplyr::select(chrom_num, LIST, TAXONCITED, chromosome_count, notes, unambiguous_match, matched_by_cit., match_to_subsp_etc.)
 
-Chrom_num = dplyr::rename(Chrom_num, taxon_name = LIST, cited_name = TAXONCITED ) 
+chrom_num = dplyr::rename(chrom_num, taxon_name = LIST, cited_name = TAXONCITED ) 
 
 
 #Genome size
@@ -71,14 +149,15 @@ GS_CValueDB$ID = "4"
 
 #Genome size full
 
-GS_BI = dplyr::bind_rows(GS_Kew, )
+GS_BI = dplyr::bind_rows(GS_Kew, GS_Smarda_2019, GS_Zonneveld_2019, GS_CValueDB)
 
+GS_BI = dplyr::select(GS_BI, taxon_name, taxon_name_Stace, authors, GS_2C_pg, GS_1C_pg, GS_2C_Mbp, GS_1C_Mbp, from.GB.material, Data.source, ID)
 
 
 #Add final datafiles to project
 usethis::use_data(BIlist, overwrite = TRUE)
 
-usethis::use_data(Chrom_num, overwrite = TRUE)
+usethis::use_data(chrom_num, overwrite = TRUE)
 
 usethis::use_data(GS_Kew, overwrite = TRUE)
 
@@ -87,3 +166,5 @@ usethis::use_data(GS_Smarda_2019, overwrite = TRUE)
 usethis::use_data(GS_Zonneveld_2019, overwrite = TRUE)
 
 usethis::use_data(GS_CValueDB, overwrite = TRUE)
+
+usethis::use_data(GS_BI, overwrite = TRUE)
